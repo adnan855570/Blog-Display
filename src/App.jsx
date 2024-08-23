c7320f0f33ad067dbc463f12b69741582595eee7
adnan855570 <ktk855570@gmail.com>
2024-08-23 10:22:04 +0500

    new commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import BlogList from './components/Bloglist';
 import './App.css'
+import BlogList from './components/Bloglist';
 
 const App = () => {
   return (
     <div className="App">
       <h1>Blog Posts</h1>
-      <BlogList />
+      <BlogList/>
     </div>
   );
 };
